72f9bb96960e90c386f03dc017c996ffe58d525a
Dhriti <Goyad512@newschool.edu>
2021-05-03 20:45:46 -0400

    added array.from

--- InputOutput/message/script.js
+++ InputOutput/message/script.js
@@ -1,98 +1,114 @@
 
-let clickDiv = document.querySelector("#clickDiv");
+// let clickDiv = document.querySelector("#clickDiv");
 
-//click Event 
-clickDiv.addEventListener("click", function(event){
-    console.log("Click")
-    console.log(event);
-});
+// //click Event 
+// clickDiv.addEventListener("click", function(event){
+//     console.log("Click")
+//     console.log(event);
+// });
 
-//Mousedown Event
+// //Mousedown Event
 
-clickDiv.addEventListener("mousedown", function(e){
-    console.log("Mouse down!")
-    console.log(e);
-});
+// clickDiv.addEventListener("mousedown", function(e){
+//     console.log("Mouse down!")
+//     console.log(e);
+// });
 
-//Mouseup Event
+// //Mouseup Event
 
-clickDiv.addEventListener("mouseup", function(event){
-    console.log("Mouse up!")
-    console.log(event);
-});
+// clickDiv.addEventListener("mouseup", function(event){
+//     console.log("Mouse up!")
+//     console.log(event);
+// });
 
-//Scroll message fill
+// //Scroll message fill
 
-let scrollDiv = document.querySelector("#scrollDiv");
+// let scrollDiv = document.querySelector("#scrollDiv");
 
-//Wheel (scroll) event 
-scrollDiv.addEventListener("wheel", function(event){
-    console.log("Wheel!");
-    console.log(event);
-})
+// //Wheel (scroll) event 
+// scrollDiv.addEventListener("wheel", function(event){
+//     console.log("Wheel!");
+//     console.log(event);
+// })
 
 
-//adding a cursor 
-let myCursor = document.querySelector("#customCursor");
 
-window.addEventListener("mousemove", function(event){
-    console.log(event);
-    console.log("X: ");
-    console.log(event.pageX);
-    console.log("Y: ");
-    console.log(event.pageY);
-    myCursor.style.top = (event.pageY - 20) + "px";
-    myCursor.style.left = (event.pageX  - 20) + "px";
-});
+
+
+
+// //adding a cursor 
+// let myCursor = document.querySelector("#customCursor");
+
+// window.addEventListener("mousemove", function(event){
+//     console.log(event);
+//     console.log("X: ");
+//     console.log(event.pageX);
+//     console.log("Y: ");
+//     console.log(event.pageY);
+//     myCursor.style.top = (event.pageY - 20) + "px";
+//     myCursor.style.left = (event.pageX  - 20) + "px";
+// });
+
+
+
+
+
 
 //painter 
-let container = document.querySelector(".grid-container");
+// let container = document.querySelector(".grid-container");
 
-let clearDrawing = document.querySelector("#clearDrawing");
-let isPainting = false;
+// let clearDrawing = document.querySelector("#clearDrawing");
+// let isPainting = false;
 
-for (let i = 0;  i < 30 * 30; i ++){
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("pixel");
+// for (let i = 0;  i < 30 * 30; i ++){
+//     let newDiv = document.createElement("div");
+//     newDiv.classList.add("pixel");
 
-    // newDiv.addEventListener("click", function(){
-    //     newDiv.style.backgroundColor = "black";
-    // })
+//     // newDiv.addEventListener("click", function(){
+//     //     newDiv.style.backgroundColor = "black";
+//     // })
 
-     newDiv.addEventListener("mousedown", function(){
-        isPainting = true;
-        newDiv.style.backgroundColor = "black";
-    });
+//      newDiv.addEventListener("mousedown", function(){
+//         isPainting = true;
+//         newDiv.style.backgroundColor = "black";
+//     });
 
-    newDiv.addEventListener("mouseenter", function(){
-        newDiv.style.backgroundColor = "black";
-    });
+//     newDiv.addEventListener("mouseenter", function(){
+//         newDiv.style.backgroundColor = "black";
+//     });
 
-    newDiv.addEventListener("mouseup", function(){
-        isPainting = false;
-    });
+//     newDiv.addEventListener("mouseup", function(){
+//         isPainting = false;
+//     });
     
-    clearDrawing.addEventListener("click", function(){
-        newDiv.style.background = "white";
-    })
+//     clearDrawing.addEventListener("click", function(){
+//         newDiv.style.background = "white";
+//     })
 
-    container.appendChild(newDiv);
-}
+//     container.appendChild(newDiv);
+// }
+
+
+
+
+
 
 //key pressing down
 
-let result = document.querySelector("#result");
+// let result = document.querySelector("#result");
 
-document.addEventListener("keydown", function(event){
-    console.log(event);
-    console.log("what did we just press:")
-    console.log(event.key)
-    if (event.key == "Enter"){
-        document.body.classList.toggle("blue");
-    }
+// document.addEventListener("keydown", function(event){
+//     console.log(event);
+//     console.log("what did we just press:")
+//     console.log(event.key)
+//     if (event.key == "Enter"){
+//         document.body.classList.toggle("blue");
+//     }
 
-    result.innerText = "You just pressed: " + event.key;
-})
+//     result.innerText = "You just pressed: " + event.key;
+// })
+
+
 
 
 
@@ -117,24 +133,69 @@ inputElement.addEventListener("keydown", function(event){
 //adding songs 
 
 let chords = {
-    'abc': 'mini-exported 2.mp3',
+    'Aa': 'amaj1.mp3',
+    'Bb': 'amaj2.mp3',
+    'Cc': 'amaj3.mp3',
+    'Dd': 'amaj4.mp3',
+    'Ee': 'amaj5.mp3',
+    'Ff': 'amin1.mp3',
+    'Gg': 'amin2.mp3',
+    'Hh': 'amin3.mp3',
+    'Ii': 'amin4.mp3',
+    'Jj': 'amin5.mp3',
+    'Kk': 'amin6.mp3',
+    'Ll': 'amin7.mp3',
+    'Mm': 'cmaj1.mp3',
+    'Nn': 'cmaj2.mp3',
+    'Oo': 'cmaj3.mp3',
+    'Pp': 'cmaj4.mp3',
+    'Qq': 'cmaj5.mp3',
+    'Rr': 'cmaj6.mp3',
+    'Ss': 'cmaj7.mp3',
+    'Tt': 'dmaj1.mp3',
+    'Uu': 'dmaj2.mp3',
+    'Vv': 'dmaj3.mp3',
+    'Ww': 'dmaj4.mp3',
+    'Xx': 'dmaj5.mp3',
+    'Yy': 'emaj1.mp3',
+    'Zz': 'emaj2.mp3',
+
+    '148': 'emaj3.mp3',
+    '259': 'emaj4.mp3',
+    '360': 'emaj5.mp3',
+
+
+    ',./': 'fmaj1.mp3',
+    ';:"': 'fmaj2.mp3',
+    '-()+': 'fmaj3.mp3',
+    '*&^#!': 'fmaj4.mp3',
+    '~–|': 'fmaj5.mp3',
+    '=@': 'fmaj6.mp3',
 
 };
 
-let input = document.querySelector('input');
+let output = document.querySelector('#result');
+
+let input = document.querySelector('#input');
 let audioChords = [];
 
 function playChord(event) {
 
     if (event.key == 'Enter') {
+       
         let ourText = input.value;
-        ourText.forEach((key) => {
+        
+        Array.from(ourText).forEach((key) => {
             let chord = Object.keys(chords).find(k => k.includes(key));
             if (chord) {
                 let audio = document.createElement('audio');
                 audio.src = chords[chord];
                 audio.play();
                 audioChords.push(audio);
+
+                let img = document.createElement('img');
+                img.scr = `${chord}.png`; // Aa.img
+                result.append(img);
             }
         });
     }
